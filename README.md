be60a6b6994ec34d88e893fe3795c970f68239ac
Adam Gross <adamgross42@gmail.com>
2015-10-07 09:21:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Real Time Updates Samples
+
+These are sample clients for [Facebook's Real Time Updates API](https://developers.facebook.com/…/graph-…/real-time-updates/) and [Instagram's Real-time Photo Updates API](https://instagram.com/developer/realtime/).
---
+++ parse/cloud/main.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
+});
+
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.param('hub.mode') == 'subscribe' &&
+    req.param('hub.verify_token') == 'token'
+  ) {
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.send(400);
+  }
+});
+
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:');
+  console.log(req.body);
+  // Process the Facebook updates here
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+});
+
+app.listen();
