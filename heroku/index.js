82ae5a11b508c2cefe205351ebe8e48eb99980bd
Mohsen Agsen <mohsena@fb.com>
2016-06-22 09:12:04 -0700

    Ensure xhub is used in express before bodyParser

--- heroku/index.js
+++ heroku/index.js
@@ -14,8 +14,8 @@ var xhub = require('express-x-hub');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(bodyParser.json());
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   console.log(req);
